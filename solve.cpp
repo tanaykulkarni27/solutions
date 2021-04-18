5000f658e10cadee1bad7bfe7e96bf01af4d1fd2
tanaykulkarni27gi8t config --global user.email 1002kulkarnitp@gmail.comgit config --global user.name <1002kulkarnitp@gmail.com>
2021-04-18 10:58:39 +0530

    Add solutions

---
+++ solve.cpp
@@ -0,0 +1,32 @@
+/*
+	Author :- Tanay Kulkarni
+	Date   :- 18-4-2021
+	Time   :- 10:55:16.860422
+	Name   :- solve.cpp 
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void debug(string j){
+cout<<"| Value : "<<j<<" |\n";
+}
+void debug(int j){
+cout<<"| Value : "<<j<<" |\n";
+}
+void debug(int i,string j){
+cout<<"Iteration "<<i<<": "<<j<<"\n";
+}
+void debug(int i,int j){
+cout<<"Iteration "<<i<<": "<<j<<"\n";
+}
+void testcase(){
+puts("");
+}
+int main(){
+int t;
+cin>>t;
+for(int i = 1;i<=t;i++){
+		cout<<"Case #"<<i<<": ";
+		testcase();
+}
+return 0;
+}
