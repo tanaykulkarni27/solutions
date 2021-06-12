8ad7b59b914cfaa13e2a75cf0a80bd7b7ec1dda6
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-06-12 21:28:58 +0530

    Create b.cpp

---
+++ codeforces/686/b.cpp
@@ -0,0 +1,63 @@
+/*
+	* Author :- Tanay Kulkarni
+	* Date   :- 12-6-2021
+	* Time   :- 19:34:15.787477
+	* Name   :- solve.cpp
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+void testcase(){
+	int n;
+	cin>>n;
+	vector<int>arr(n+1,0);
+	map<int,int>ms;
+	for(int i = 0;i<n;i++){
+		int x;
+		cin>>x;
+		arr[x]++;
+		ms[x] = i+1;
+	}
+	for(int i = 1;i<=n;i++){
+		if(arr[i] == 1){
+			cout<<ms[i]<<"\n";
+			return;
+		}
+	}
+	cout<<"-1";
+	puts("");
+}
+int main(){
+int t;
+cin>>t;
+for(int i = 1;i<=t;i++){
+		// cout<<"Case #"<<i<<": ";
+		testcase();
+}
+return 0;
+}
